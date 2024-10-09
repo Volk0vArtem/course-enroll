f177c3fa62e0a613e78308982be0fb36797b7a05
Artem <artem33volkov@gmail.com>
2024-10-09 11:28:22 +0300

    initial commit

---
+++ src/main/java/com/custis/App.java
@@ -0,0 +1,13 @@
+package com.custis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
